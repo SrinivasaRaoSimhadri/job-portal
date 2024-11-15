e2d49bc811d742aec422a37fd653774119d7ee3a
SrinivasaRaoSimhadri <sinivasaraosimhadri01@gmail.com>
2024-11-15 15:20:45 +0530

    initial commit

--- frontend/src/layout/Footer.jsx
+++ frontend/src/layout/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="text-center py-4 px-6 bg bg-gray-900">
-            Copyright © {new Date().getFullYear()} - All right reserved
+            Copyright©{new Date().getFullYear()} - All right reserved
         </div>
     )
 }
